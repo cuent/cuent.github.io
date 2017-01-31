7ff64ae763ae6277ac2fe56a6dd711764decc8eb
cuent <c.uent@hotmail.com>
2017-01-31 00:16:40 -0500

    hp message

---
+++ happybirthday/js/functions_dev.js
@@ -0,0 +1,61 @@
+// variables
+var $window = $(window)
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+
+$(window).resize(function() {
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#gallery").height() - $("#code").height()) / 2);
+}
+
+(function($) {
+	$.fn.showPhotos = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 2100);
+		});
+		return this;
+	};
+})(jQuery);
